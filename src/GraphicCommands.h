14365db946cdb6129a22d5128a6b65e6cbd6b1fa
Kacper Kozerski <01158764@pw.edu.pl>
2022-11-05 12:35:31 +0100

    Added new interface for GraphicCommand - now using visitor

--- src/GraphicCommands.h
+++ src/GraphicCommands.h
@@ -7,6 +7,7 @@
 #include "EPSCommandRepresentation.h"
 #include <memory>
 
+class Visitor;
 class GraphicCommand : public Command {
     Point startingPoint;
     //represents point, to which drawing cursor is moved after the command
@@ -16,8 +17,22 @@ public:
     Point getStartingPoint() const { return startingPoint; }
     Point getMovePoint() const { return movePoint; }
     virtual std::string toString() const = 0;
-    virtual double difference(const GraphicCommand & com) const = 0;
-    // creates Graphic Command that represents midpoint between two other commands
-    virtual std::unique_ptr<GraphicCommand> createMidpoint( const GraphicCommand & com ) const = 0;
+    virtual double accept( const Visitor & dv );
 };
+
+class LeftOrientedLineCommand : public GraphicCommand {
+
+    LeftOrientedLineCommand( const Point & start, const Point & move ) : GraphicCommand( start, move ) {}
+};
+
+class RightOrientedLineCommand : public GraphicCommand {
+    RightOrientedLineCommand( const Point & start, const Point & move ) : GraphicCommand( start, move ) {}
+};
+
+class PointCommand: public GraphicCommand {
+    PointCommand( const Point & coord ) : GraphicCommand( coord, coord ) {}
+};
+
+
+
 #endif //ZPR_GRAPHICCOMMANDS_H
