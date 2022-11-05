9bdee227e35e7de4ecd2abe81e617f7e26c8df25
Kacper Kozerski <01158764@pw.edu.pl>
2022-11-05 13:02:16 +0100

    Added Difference Visitor interfaces - without implementation

--- src/GraphicCommands.h
+++ src/GraphicCommands.h
@@ -6,8 +6,11 @@
 #define ZPR_GRAPHICCOMMANDS_H
 #include "EPSCommandRepresentation.h"
 #include <memory>
-
-class Visitor;
+class GraphicCommand;
+class DifferenceVisitor {
+public:
+    virtual double visit(const GraphicCommand & gc) const = 0;
+};
 class GraphicCommand : public Command {
     Point startingPoint;
     //represents point, to which drawing cursor is moved after the command
@@ -17,22 +20,51 @@ public:
     Point getStartingPoint() const { return startingPoint; }
     Point getMovePoint() const { return movePoint; }
     virtual std::string toString() const = 0;
-    virtual double accept( const Visitor & dv );
+    virtual double acceptDifferenceVisitor( const DifferenceVisitor & v ) { return v.visit(*this); }
+
 };
 
 class LeftOrientedLineCommand : public GraphicCommand {
 
     LeftOrientedLineCommand( const Point & start, const Point & move ) : GraphicCommand( start, move ) {}
+    virtual std::string toString() const { /* @todo implement*/ return ""; }
 };
 
 class RightOrientedLineCommand : public GraphicCommand {
     RightOrientedLineCommand( const Point & start, const Point & move ) : GraphicCommand( start, move ) {}
+    virtual std::string toString() const { /* @todo implement*/ return ""; }
 };
 
 class PointCommand: public GraphicCommand {
     PointCommand( const Point & coord ) : GraphicCommand( coord, coord ) {}
+    virtual std::string toString() const { /* @todo implement*/ return ""; }
 };
 
+class LeftLineDifferenceVisitor : public DifferenceVisitor {
+    const LeftOrientedLineCommand & ll_;
+public:
+    LeftLineDifferenceVisitor(const LeftOrientedLineCommand & ll ) : ll_( ll ) {}
+    virtual double visit( const LeftOrientedLineCommand & ll ) { /* @todo implement*/ return 0; }
+    virtual double visit( const RightOrientedLineCommand & rl ) { /* @todo implement*/ return 0; }
+    virtual double visit( const PointCommand & p ) { /* @todo implement*/ return 0; }
+};
 
+class RightLineDifferenceVisitor : public DifferenceVisitor {
+    const RightOrientedLineCommand & rl_;
+public:
+    RightLineDifferenceVisitor(const RightOrientedLineCommand & rl ) : rl_( rl ) {}
+    virtual double visit( const LeftOrientedLineCommand & ll ) { /* @todo implement*/ return 0; }
+    virtual double visit( const RightOrientedLineCommand & rl ) { /* @todo implement*/ return 0; }
+    virtual double visit( const PointCommand & p ) { /* @todo implement*/ return 0; }
+};
+
+class PointDifferenceVisitor : public DifferenceVisitor {
+    const PointCommand & p_;
+public:
+    PointDifferenceVisitor(const PointCommand & p ) : p_( p ) {}
+    virtual double visit( const LeftOrientedLineCommand & ll ) { /* @todo implement*/ return 0; }
+    virtual double visit( const RightOrientedLineCommand & rl ) { /* @todo implement*/ return 0; }
+    virtual double visit( const PointCommand & p ) { /* @todo implement*/ return 0; }
+};
 
 #endif //ZPR_GRAPHICCOMMANDS_H
