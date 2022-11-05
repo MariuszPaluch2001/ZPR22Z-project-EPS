11d77b11b558c2530dc20127f02c72ce796c9050
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-11-05 19:50:11 +0100

    Add PictureEPS files.

---
+++ src/PictureEPS.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mariusz on 05.11.22.
+//
+
+#include "PictureEPS.h"
---
+++ src/PictureEPS.h
@@ -0,0 +1,14 @@
+//
+// Created by mariusz on 05.11.22.
+//
+
+#ifndef ZPR_PICTUREEPS_H
+#define ZPR_PICTUREEPS_H
+
+
+class PictureEPS {
+
+};
+
+
+#endif //ZPR_PICTUREEPS_H
