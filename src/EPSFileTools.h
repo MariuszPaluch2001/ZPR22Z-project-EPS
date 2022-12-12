4167b9113aac4a93b46d0f1c17203394d1fd05ad
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-12-12 13:52:31 +0100

    Add comments in EPSFIleTools.h.

--- src/EPSFileTools.h
+++ src/EPSFileTools.h
@@ -1,7 +1,11 @@
 //
 // Created by mariusz on 08.11.22.
 //
-
+/*
+    File EPSFileTools.h contains class definitions representing the File Header,
+    and classes representing input and output files. They are used to read text from
+    input, pass data to algorithm and send data to output file.
+*/
 #ifndef ZPR_EPSFILETOOLS_H
 #define ZPR_EPSFILETOOLS_H
 #include <fstream>
@@ -31,6 +35,12 @@ public:
   Resolution getResolution() const { return resolution_; }
 };
 
+/*
+    This class represent input file. As arguments in constructor class require
+    reference to input stream. Before reading commands, class's object should read header by
+    call method 'getHeader', otherwise method getCommand throw runtime exception.
+    Class don't close the input stream.
+*/
 class EPSInFileStream {
   std::istream &file_;
   bool was_header_read = false;
@@ -49,6 +59,12 @@ public:
   bool isFinished() { return file_.peek() == EOF; }
 };
 
+/*
+    This class represent output file. As arguments in constructor class require
+    reference to output stream. Before writing commands, class's object should write header by
+    call method 'putHeader', otherwise method putCommand throw runtime exception.
+    Class don't close the output stream.
+*/
 class EPSOutFileStream {
   std::ostream &file_;
   bool was_header_write = false;
