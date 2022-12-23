c0b8add8022b0aac2c0cd4a6a03b12b17fbbb08a
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-12-23 17:53:04 +0100

    Add basic GUI layout.

--- src/GUI.cc
+++ src/GUI.cc
@@ -10,17 +10,55 @@ bool App::OnInit()
 Frame::Frame(const wxString& title, const wxPoint& pos, const wxSize& size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-  wxMenu* menuFile = new wxMenu();
+
+  menuFile = new wxMenu();
   menuFile->Append(wxID_EXIT);
 
-  wxMenu* menuHelp = new wxMenu();
+  menuHelp = new wxMenu();
   menuHelp->Append(wxID_ABOUT);
 
-  wxMenuBar* menuBar = new wxMenuBar();
+  menuBar = new wxMenuBar();
   menuBar->Append(menuFile, "&File");
   menuBar->Append(menuHelp, "&Help");
 
   SetMenuBar(menuBar);
+  
+  sizer = new wxBoxSizer(wxVERTICAL);
+  
+  label1 = new wxStaticText(this, wxID_ANY, "Wybierz pomniejszenie obrazka:     ");
+  comboBox = new wxComboBox(this, wxID_ANY, "", wxDefaultPosition, wxSize(150, 30), 0, nullptr, wxCB_READONLY);
+
+  for (int i = 1; i <= 5; i++)
+  {
+        comboBox->Append(wxString::Format("%dx", i));
+  }
+
+  comboBox->SetSelection(0);
+  
+  rowSizer1 = new wxBoxSizer(wxHORIZONTAL);
+  rowSizer1->Add(label1, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
+  rowSizer1->Add(comboBox, 1, wxEXPAND);
+  
+  label2 = new wxStaticText(this, wxID_ANY, "Min odleglosc pomiedzy punktami:");
+  textCtrl = new wxTextCtrl(this, wxID_ANY, "0.0", wxDefaultPosition, wxSize(150, 30), wxTE_PROCESS_ENTER);
+  
+  rowSizer2 = new wxBoxSizer(wxHORIZONTAL);
+  rowSizer2->Add(label2, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
+  rowSizer2->Add(textCtrl, 1, wxEXPAND);
+  
+  sizerButtons = new wxBoxSizer(wxVERTICAL);
+  buttonSelectFile = new wxButton(this, wxID_ANY, "Wybierz plik", wxDefaultPosition);
+  buttonSubmit = new wxButton(this, wxID_ANY, "Potwierdz", wxDefaultPosition);
+  
+  sizerButtons->Add(buttonSelectFile, 0, wxEXPAND | wxALL, 5);
+  sizerButtons->Add(buttonSubmit, 0, wxEXPAND | wxALL, 5);
+  
+  sizer->Add(rowSizer1, 0, wxALL, 10);
+  sizer->Add(rowSizer2, 0, wxALL, 10);
+  sizer->Add(sizerButtons, 0, wxALL, 10);
+  
+  SetSizer(sizer);
+
   CreateStatusBar();
   SetStatusText("Welcome to app!");
 }
--- src/GUI.h
+++ src/GUI.h
@@ -16,9 +16,22 @@ public:
   Frame(const wxString& title, const wxPoint& pos, const wxSize& size);
 
 private:
+  wxMenu* menuFile;
+  wxMenu* menuHelp;
+  wxMenuBar* menuBar;
+  wxBoxSizer* sizer;
+  wxBoxSizer* rowSizer1;
+  wxBoxSizer* rowSizer2;
+  wxBoxSizer* sizerButtons;
+  wxButton* buttonSubmit;
+  wxButton* buttonSelectFile;
+  wxStaticText* label1;
+  wxStaticText* label2;
+  wxComboBox* comboBox;
+  wxTextCtrl* textCtrl;
+  
   void OnExit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
-  void OnHello(wxCommandEvent& event);
 
   wxDECLARE_EVENT_TABLE();
 };
