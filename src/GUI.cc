db1da5548413e9e08ad80bd4fc8e328b1bd6ceeb
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2023-01-05 19:38:09 +0100

    Replace raw pointers in GUI by unique pointers.

--- src/GUI.cc
+++ src/GUI.cc
@@ -13,7 +13,7 @@ Frame::Frame(const wxString &title, const wxPoint &pos, const wxSize &size)
 
   initMenuBar();
 
-  SetMenuBar(menu_bar);
+  SetMenuBar(menu_bar.get());
 
   initComboBoxScale();
 
@@ -29,27 +29,20 @@ Frame::Frame(const wxString &title, const wxPoint &pos, const wxSize &size)
 
   initSizer();
 
-  SetSizer(sizer);
+  SetSizer(sizer.get());
 }
 
 wxBEGIN_EVENT_TABLE(Frame, wxFrame) EVT_MENU(wxID_EXIT, Frame::onExit)
     EVT_MENU(wxID_ABOUT, Frame::onAbout) wxEND_EVENT_TABLE()
 
 void Frame::initMenuBar() {
-  menu_file = new wxMenu();
   menu_file->Append(wxID_EXIT);
-  menu_help = new wxMenu();
   menu_help->Append(wxID_ABOUT);
-  menu_bar = new wxMenuBar();
-  menu_bar->Append(menu_file, "&File");
-  menu_bar->Append(menu_help, "&Help");
+  menu_bar->Append(menu_file.get(), "&File");
+  menu_bar->Append(menu_help.get(), "&Help");
 }
 
 void Frame::initComboBoxScale() {
-  label_scale_input =
-      new wxStaticText(this, wxID_ANY, "Wybierz pomniejszenie obrazka:     ");
-  combo_box_scale = new wxComboBox(this, wxID_ANY, "", wxDefaultPosition,
-                                   wxSize(150, 30), 0, nullptr, wxCB_READONLY);
 
   for (int i = 1; i <= 5; i++) {
     combo_box_scale->Append(wxString::Format("%dx", i));
@@ -57,107 +50,67 @@ void Frame::initComboBoxScale() {
 
   combo_box_scale->SetSelection(0);
 
-  row_sizer1 = new wxBoxSizer(wxHORIZONTAL);
-  row_sizer1->Add(label_scale_input, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
-  row_sizer1->Add(combo_box_scale, 1, wxEXPAND);
+  row_sizer1->Add(label_scale_input.get(), 0, wxALIGN_CENTER_VERTICAL + wxRIGHT, 5);
+  row_sizer1->Add(combo_box_scale.get(), 1, wxEXPAND);
 }
 
 void Frame::initInputMinDist() {
-  label_input_min_dist =
-      new wxStaticText(this, wxID_ANY, "Min odleglosc pomiedzy punktami:");
-  min_dist_ctrl = new wxTextCtrl(this, wxID_ANY, "0.0", wxDefaultPosition,
-                                 wxSize(150, 30), wxTE_PROCESS_ENTER);
 
-  row_sizer2 = new wxBoxSizer(wxHORIZONTAL);
-  row_sizer2->Add(label_input_min_dist, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT,
+  row_sizer2->Add(label_input_min_dist.get(), 0, wxALIGN_CENTER_VERTICAL + wxRIGHT,
                   5);
-  row_sizer2->Add(min_dist_ctrl, 1, wxEXPAND);
+  row_sizer2->Add(min_dist_ctrl.get(), 1, wxEXPAND);
 }
 
 void Frame::initSortingRange() {
-  label_sorting_range = new wxStaticText(
-      this, wxID_ANY, "Wybierz zakres sortowania:                ");
-  combo_box_sorting_range =
-      new wxComboBox(this, wxID_ANY, "", wxDefaultPosition, wxSize(150, 30), 0,
-                     nullptr, wxCB_READONLY);
-
   for (int i = 1; i <= 100; i++) {
     combo_box_sorting_range->Append(wxString::Format("%d", i));
   }
 
   combo_box_sorting_range->SetSelection(49);
 
-  row_sizer3 = new wxBoxSizer(wxHORIZONTAL);
-  row_sizer3->Add(label_sorting_range, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
-  row_sizer3->Add(combo_box_sorting_range, 1, wxEXPAND);
+  row_sizer3->Add(label_sorting_range.get(), 0, wxALIGN_CENTER_VERTICAL + wxRIGHT, 5);
+  row_sizer3->Add(combo_box_sorting_range.get(), 1, wxEXPAND);
 }
 
 void Frame::initButtonsRow() {
-  sizer_buttons = new wxBoxSizer(wxHORIZONTAL);
-  button_select_file = new wxButton(this, wxID_ANY, "Wybierz plik", wxDefaultPosition);
   button_select_file->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Frame::chooseFile,
                            this);
 
-  button_submit = new wxButton(this, wxID_ANY, "Potwierdz", wxDefaultPosition);
   button_submit->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Frame::submit, this);
-  sizer_buttons->Add(button_select_file, 0, wxEXPAND | wxRIGHT | wxUP | wxDOWN,
+  sizer_buttons->Add(button_select_file.get(), 0, wxEXPAND + wxRIGHT + wxUP + wxDOWN,
                      5);
-  sizer_buttons->Add(button_submit, 0, wxEXPAND | wxALL, 5);
+  sizer_buttons->Add(button_submit.get(), 0, wxEXPAND + wxALL, 5);
 }
 
 void Frame::initImages() {
-  sizer_images = new wxBoxSizer(wxVERTICAL);
+  sizer_input_image->Add(label_input_image.get(), 0, wxEXPAND + wxALL, 5);
+  sizer_input_image->Add(input_image.get(), 0, wxEXPAND + wxALL, 5);
 
-  sizer_input_image = new wxBoxSizer(wxVERTICAL);
-  label_input_image = new wxStaticText(this, wxID_ANY, "Obrazek wejsciowy");
-  input_image = new wxStaticBitmap(
-      this, wxID_ANY,
-      wxBitmap("assets/no_eps_available.png", wxBITMAP_TYPE_PNG));
-  label_input_image_size = new wxStaticText(this, wxID_ANY, "Rozmiar przed: ");
-  label_input_image_resolution =
-      new wxStaticText(this, wxID_ANY, "Rozdzielczosc przed: ");
+  sizer_output_image->Add(label_output_image.get(), 0, wxEXPAND + wxALL, 5);
+  sizer_output_image->Add(output_image.get(), 0, wxEXPAND + wxALL, 5);
 
-  sizer_input_image->Add(label_input_image, 0, wxEXPAND | wxALL, 5);
-  sizer_input_image->Add(input_image, 0, wxEXPAND | wxALL, 5);
-
-  sizer_output_image = new wxBoxSizer(wxVERTICAL);
-  label_output_image = new wxStaticText(this, wxID_ANY, "Obrazek wyjsciowy");
-  output_image = new wxStaticBitmap(
-      this, wxID_ANY,
-      wxBitmap("assets/no_eps_available.png", wxBITMAP_TYPE_PNG));
-  label_output_image_size = new wxStaticText(this, wxID_ANY, "Rozmiar po: ");
-  label_output_image_resolution =
-      new wxStaticText(this, wxID_ANY, "Rozdzielczosc po: ");
-
-  sizer_output_image->Add(label_output_image, 0, wxEXPAND | wxALL, 5);
-  sizer_output_image->Add(output_image, 0, wxEXPAND | wxALL, 5);
-
-  sizer_images->Add(sizer_input_image, 0, wxEXPAND | wxRIGHT | wxUP | wxDOWN,
+  sizer_images->Add(sizer_input_image.get(), 0, wxEXPAND + wxRIGHT + wxUP + wxDOWN,
                     5);
-  sizer_images->Add(sizer_output_image, 0, wxEXPAND | wxALL, 5);
+  sizer_images->Add(sizer_output_image.get(), 0, wxEXPAND + wxALL, 5);
 }
 
 void Frame::initButtonGetOutput() {
-  button_get_output = new wxButton(this, wxID_ANY, "Zapisz", wxDefaultPosition);
   button_get_output->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Frame::save, this);
 }
 
 void Frame::initSizer() {
-  sizer = new wxBoxSizer(wxHORIZONTAL);
-  sizer_left = new wxBoxSizer(wxVERTICAL);
-  sizer_right =  new wxBoxSizer(wxVERTICAL);
-  sizer_left->Add(row_sizer1, 0, wxALL, 10);
-  sizer_left->Add(row_sizer2, 0, wxALL, 10);
-  sizer_left->Add(row_sizer3, 0, wxALL, 10);
-  sizer_left->Add(sizer_buttons, 0, wxALL, 10);
-  sizer_right->Add(sizer_images, 0, wxALL, 10);
-  sizer_left->Add(button_get_output, 0, wxALL, 10);
-  sizer_left->Add(label_input_image_size, 0, wxALL, 10);
-  sizer_left->Add(label_input_image_resolution, 0, wxALL, 10);
-  sizer_left->Add(label_output_image_size, 0, wxALL, 10);
-  sizer_left->Add(label_output_image_resolution, 0, wxALL, 10);
-  sizer->Add(sizer_left, 0, wxALL, 10);
-  sizer->Add(sizer_right, 0, wxALL, 10);
+  sizer_left->Add(row_sizer1.get(), 0, wxALL, 10);
+  sizer_left->Add(row_sizer2.get(), 0, wxALL, 10);
+  sizer_left->Add(row_sizer3.get(), 0, wxALL, 10);
+  sizer_left->Add(sizer_buttons.get(), 0, wxALL, 10);
+  sizer_right->Add(sizer_images.get(), 0, wxALL, 10);
+  sizer_left->Add(button_get_output.get(), 0, wxALL, 10);
+  sizer_left->Add(label_input_image_size.get(), 0, wxALL, 10);
+  sizer_left->Add(label_input_image_resolution.get(), 0, wxALL, 10);
+  sizer_left->Add(label_output_image_size.get(), 0, wxALL, 10);
+  sizer_left->Add(label_output_image_resolution.get(), 0, wxALL, 10);
+  sizer->Add(sizer_left.get(), 0, wxALL, 10);
+  sizer->Add(sizer_right.get(), 0, wxALL, 10);
 }
 
 void Frame::onExit(wxCommandEvent &event) {
@@ -171,8 +124,8 @@ void Frame::onAbout(wxCommandEvent &event) {
 
 void Frame::chooseFile(wxCommandEvent &event) {
     wxBitmap newBitmap;
-    auto openFileDialog = new wxFileDialog(this, "Otwórz plik", "", "",
-                                                    "Pliki eps (*.eps)|*.eps", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+    std::unique_ptr<wxFileDialog> openFileDialog{new wxFileDialog(this, "Otwórz plik", "", "",
+                                                                  "Pliki eps (*.eps)|*.eps", wxFD_OPEN | wxFD_FILE_MUST_EXIST)};
 
     if (openFileDialog->ShowModal() == wxID_CANCEL)
         return;
--- src/GUI.h
+++ src/GUI.h
@@ -2,7 +2,7 @@
 #define ZPR_GUI_H
 #include <sstream>
 #include <wx/wxprec.h>
-
+#include <memory>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
@@ -12,41 +12,56 @@ public:
   bool OnInit() override;
 };
 
+using PwxMenu = std::unique_ptr<wxMenu>;
+using PwxMenuBar = std::unique_ptr<wxMenuBar>;
+using PwxBoxSizer = std::unique_ptr<wxBoxSizer>;
+using PwxButton = std::unique_ptr<wxButton>;
+using PwxStaticText = std::unique_ptr<wxStaticText>;
+using PwxComboBox = std::unique_ptr<wxComboBox>;
+using PwxTextCtrl = std::unique_ptr<wxTextCtrl>;
+using PwxStaticBitmap = std::unique_ptr<wxStaticBitmap>;
+
+
 class Frame : public wxFrame {
 public:
   Frame(const wxString &title, const wxPoint &pos, const wxSize &size);
 
 private:
-  wxMenu *menu_file{};
-  wxMenu *menu_help{};
-  wxMenuBar *menu_bar{};
-  wxBoxSizer *sizer{};
-  wxBoxSizer *sizer_left{};
-  wxBoxSizer *sizer_right{};
-  wxBoxSizer *row_sizer1{};
-  wxBoxSizer *row_sizer2{};
-  wxBoxSizer *row_sizer3{};
-  wxBoxSizer *sizer_buttons{};
-  wxBoxSizer *sizer_images{};
-  wxBoxSizer *sizer_input_image{};
-  wxBoxSizer *sizer_output_image{};
-  wxButton *button_submit{};
-  wxButton *button_select_file{};
-  wxButton *button_get_output{};
-  wxStaticText *label_scale_input{};
-  wxStaticText *label_input_min_dist{};
-  wxStaticText *label_sorting_range{};
-  wxStaticText *label_input_image{};
-  wxStaticText *label_output_image{};
-  wxStaticText *label_input_image_size{};
-  wxStaticText *label_output_image_size{};
-  wxStaticText *label_input_image_resolution{};
-  wxStaticText *label_output_image_resolution{};
-  wxComboBox *combo_box_scale{};
-  wxComboBox *combo_box_sorting_range{};
-  wxTextCtrl *min_dist_ctrl{};
-  wxStaticBitmap *input_image{};
-  wxStaticBitmap *output_image{};
+  PwxMenu menu_file{new wxMenu};
+  PwxMenu menu_help{new wxMenu};
+  PwxMenuBar menu_bar{new wxMenuBar};
+  PwxBoxSizer sizer{new wxBoxSizer(wxHORIZONTAL)};
+  PwxBoxSizer sizer_left{new wxBoxSizer(wxVERTICAL)};
+  PwxBoxSizer sizer_right{new wxBoxSizer(wxVERTICAL)};
+  PwxBoxSizer row_sizer1{new wxBoxSizer(wxHORIZONTAL)};
+  PwxBoxSizer row_sizer2{new wxBoxSizer(wxHORIZONTAL)};
+  PwxBoxSizer row_sizer3{new wxBoxSizer(wxHORIZONTAL)};
+  PwxBoxSizer sizer_buttons{new wxBoxSizer(wxHORIZONTAL)};
+  PwxBoxSizer sizer_images{new wxBoxSizer(wxVERTICAL)};
+  PwxBoxSizer sizer_input_image{new wxBoxSizer(wxVERTICAL)};
+  PwxBoxSizer sizer_output_image{new wxBoxSizer(wxVERTICAL)};
+  PwxButton button_submit{new wxButton(this, wxID_ANY, "Potwierdz", wxDefaultPosition)};
+  PwxButton button_select_file{new wxButton(this, wxID_ANY, "Wybierz plik", wxDefaultPosition)};
+  PwxButton button_get_output{new wxButton(this, wxID_ANY, "Zapisz", wxDefaultPosition)};
+  PwxStaticText label_scale_input{new wxStaticText(this, wxID_ANY, "Wybierz pomniejszenie obrazka:     ")};
+  PwxStaticText label_input_min_dist{new wxStaticText(this, wxID_ANY, "Min odleglosc pomiedzy punktami:")};
+  PwxStaticText label_sorting_range{new wxStaticText(this, wxID_ANY, "Wybierz zakres sortowania:                ")};
+  PwxStaticText label_input_image{new wxStaticText(this, wxID_ANY, "Obrazek wejsciowy")};
+  PwxStaticText label_output_image{new wxStaticText(this, wxID_ANY, "Obrazek wyjsciowy")};
+  PwxStaticText label_input_image_size{new wxStaticText(this, wxID_ANY, "Rozmiar przed: ")};
+  PwxStaticText label_output_image_size{new wxStaticText(this, wxID_ANY, "Rozmiar po: ")};
+  PwxStaticText label_input_image_resolution{new wxStaticText(this, wxID_ANY, "Rozdzielczosc przed: ")};
+  PwxStaticText label_output_image_resolution{new wxStaticText(this, wxID_ANY, "Rozdzielczosc po: ")};
+  PwxComboBox combo_box_scale{new wxComboBox(this, wxID_ANY, "", wxDefaultPosition,
+                                             wxSize(150, 30), 0, nullptr, wxCB_READONLY)};
+  PwxComboBox combo_box_sorting_range{ new wxComboBox(this, wxID_ANY, "", wxDefaultPosition,
+                                                      wxSize(150, 30), 0,nullptr, wxCB_READONLY)};
+  PwxTextCtrl min_dist_ctrl{new wxTextCtrl(this, wxID_ANY, "0.0", wxDefaultPosition,
+                                           wxSize(150, 30), wxTE_PROCESS_ENTER)};
+  PwxStaticBitmap input_image{new wxStaticBitmap(this, wxID_ANY,
+          wxBitmap("assets/no_eps_available.png", wxBITMAP_TYPE_PNG))};
+  PwxStaticBitmap output_image{new wxStaticBitmap(this, wxID_ANY,
+          wxBitmap("assets/no_eps_available.png", wxBITMAP_TYPE_PNG))};
   void initMenuBar();
   void initComboBoxScale();
   void initInputMinDist();
