a733bbd54e1c99fd6ccb17bcefe3bd3bc982ac91
Kacper Kozerski <01158764@pw.edu.pl>
2023-01-07 16:50:45 +0100

    Removed untracked files

--- cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kacper/Pulpit/zpr_eps
-# Build directory: /home/kacper/Pulpit/zpr_eps/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- cmake-build-debug-coverage/src/CMakeFiles/Scalar2DRepresentation.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libScalar2DRepresentation.a"
-)
